403db79f813bdef2ac844124fb3fe311379f59db
h-dung <h-dung@ruby-dev.vn>
2023-08-07 16:13:40 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import ExpenseItem from "./components/Expenses/ExpenseItem";
+import Card from "./components/UI/Card";
 
 function App() {
+  const expenses = [
+    {
+      id: 1,
+      title: "Tiamo",
+      date: new Date(2023, 8, 10),
+      amount: 123.5,
+    },
+    {
+      id: 2,
+      title: "Tiamo2",
+      date: new Date(2023, 9, 2),
+      amount: 241.5,
+    },
+    {
+      id: 3,
+      title: "Tiamo3",
+      date: new Date(2023, 7, 23),
+      amount: 653.5,
+    },
+    {
+      id: 4,
+      title: "Tiamo4",
+      date: new Date(2023, 10, 18),
+      amount: 222.5,
+    },
+    {
+      id: 5,
+      title: "Tiamo5",
+      date: new Date(2023, 6, 9),
+      amount: 874.5,
+    },
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card>
+        {expenses.map((expense, index) => (
+          <ExpenseItem key={index} {...expense} />
+        ))}
+      </Card>
     </div>
   );
 }
